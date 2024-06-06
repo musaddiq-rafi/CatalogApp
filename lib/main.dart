9eb94dce1245557a85e1d9d29113173a47b6a43f
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-06-06 23:47:39 +0600

    day 4 done

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness:  Brightness.dark,
       ),
-      initialRoute : "/home",
+      initialRoute : "/",
       routes: { 
         "/": (context) => LoginPage(),
         "/home":(context)=> HomePage(),
